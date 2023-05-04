f13e8ec426cc02fb18acca2e4cc0f50ee582266d
Martin Melecký <martinek.melecky@gmail.com>
2023-05-04 15:45:02 +0200

    nevim pomoc

---
+++ cvicenireseni.py
@@ -0,0 +1,10 @@
+#1
+even_numbers = [2, 4, 6, 8, 10]
+odd_numbers = [1, 3, 5, 7, 9]
+sjednoceni = even_numbers + odd_numbers
+print(sjednoceni)
+
+#2
+cviceni2 = [1, 3, 5, 7, 9, 11, 13, 15]
+soucet = sum(cviceni2)
+print(soucet)
